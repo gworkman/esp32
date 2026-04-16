f58185e227c7bbd1b33ee8552e8e0365a29e911a
Gus Workman <gworkman@users.noreply.github.com>
2026-04-16 23:30:16 +0200

    add package information

---
+++ CHANGELOG.md
@@ -0,0 +1,21 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to
+[Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.1.0] - 2026-04-16
+
+Initial release
+
+### Added
+
+- Support for entering bootloader mode for both UART and USB JTAG devices
+- Parsing of firmware files to validate chip family
+- Uploading stub loader and writing firmware .bin files to flash
+- Tested on ESP32C3 and ESP32C6
+
+[unreleased]: https://github.com/protolux-electronics/dither/compare/v0.2.2...HEAD
+[0.1.0]: https://github.com/gworkman/esp32/releases/tag/v0.1.0
--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Esp32.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: "Flash ESP32 microcontrollers with Elixir and Nerves",
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/gworkman/esp32"
     ]
   end
 
@@ -18,12 +21,22 @@ defmodule Esp32.MixProject do
     ]
   end
 
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README.md LICENSE CHANGELOG.md),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/gworkman/esp32"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:circuits_gpio, "~> 2.0"},
       {:circuits_uart, "~> 1.0"},
-      {:jason, "~> 1.0"}
+      {:jason, "~> 1.0"},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
     ]
   end
 end
